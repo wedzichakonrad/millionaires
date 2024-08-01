f7491e776e5594ef73e75b60396c4b703e332233
Konrad Wędzicha <progmaster2003@gmail.com>
2024-08-01 23:49:48 +0200

    started working with usecontext

--- src/components/millionaires.tsx
+++ src/components/millionaires.tsx
@@ -1,17 +1,25 @@
-import AnswersContainer from './containers/answers-container/answers-container'
-import RewardsContainer from './containers/rewards-container/rewards-container'
-import './millionaires.sass'
+import AnswersContainer from "./containers/answers-container/answers-container";
+import RewardsContainer from "./containers/rewards-container/rewards-container";
+import "./millionaires.sass";
 import { useState } from "react";
+import { createContext, useContext } from 'react';
+
+export const NotificationContext = createContext<{ isOpen: boolean}>({ isOpen: false});
 
 const Millionaires = () => {
-    const [questionNumber, setQuestionNumber] = useState<number>(0);
+  const [questionNumber, setQuestionNumber] = useState<number>(0);
 
   return (
-    <div className='millionaires'>
-        <AnswersContainer questionNumber={questionNumber} setQuestionNumber={setQuestionNumber}/>
-        <RewardsContainer questionNumber={questionNumber} />
-    </div>
-  )
-}
+      <NotificationContext.Provider value={{ isOpen: false}}>
+          <div className="millionaires">
+              <AnswersContainer
+                  questionNumber={questionNumber}
+                  setQuestionNumber={setQuestionNumber}
+              />
+              <RewardsContainer questionNumber={questionNumber} />
+          </div>
+      </NotificationContext.Provider>
+  );
+};
 
-export default Millionaires
+export default Millionaires;
